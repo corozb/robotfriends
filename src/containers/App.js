796b53594513540315b4aed8286059b6282aed68
cristian.orozco <cristian.orozco@globant.com>
2021-12-29 15:10:22 -0500

    incorporing redux - Provider - Connect - Midleware with logs

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,22 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from '../components/Scroll'
 
-function App() {
-  const [robots, setRobots] = useState([])
-  const [searchField, setSearchField] = useState('')
+import { connect } from 'react-redux'
+import { setSearchField } from '../actions'
 
-  const onSearchChange = (e) => setSearchField(e.target.value)
+const mapStateToProps = (state) => ({
+  searchField: state.searchField,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  onSearchChange: (e) => dispatch(setSearchField(e.target.value)),
+})
+
+function App({ searchField, onSearchChange }) {
+  const [robots, setRobots] = useState([])
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -16,7 +24,7 @@ function App() {
       .then((users) => setRobots(users))
   }, [])
 
-  const filteredRobots = robots.filter((robot) => robot.name.toLowerCase().includes(searchField))
+  const filteredRobots = robots.filter((robot) => robot.name.toLowerCase().includes(searchField.toLowerCase()))
 
   return !robots.length ? (
     <h1>Loading</h1>
@@ -31,4 +39,4 @@ function App() {
   )
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
+import { searchRobots } from './reducers'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import App from './containers/App'
 import 'tachyons'
 
+const logger = createLogger()
+const store = createStore(searchRobots, applyMiddleware(logger))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
